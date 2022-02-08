e88c6d8e054ca8b51a43f63d096cfe3ff736a8d5
sinjy <sinjy1203@gmail.com>
2022-02-08 17:54:44 +0900

    commit

--- common/other_class.py
+++ common/other_class.py
@@ -104,3 +104,65 @@ class BetterRnnlm(BaseModel):
     def reset_state(self):
         for layer in self.lstm_layers:
             layer.reset_state()
+            
+
+class Encoder:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+        
+        embed_w = (rn(V, D) / 100).astype('f')
+        lstm_wx = (rn(D, 4*H) / np.sqrt(D)).astype('f')
+        lstm_wh = (rn(H, 4*H) / np.sqrt(H)).astype('f')
+        lstm_b = np.zeros(4*H).astype('f')
+        
+        self.embed = TimeEmbedding(embed_w)
+        self.lstm = TimeLSTM(lstm_wx, lstm_wh, lstm_b, stateful=False)
+        
+        self.params = self.embed.params + self.lstm.params
+        self.grads = self.embed.grads + self.lstm.grads
+        self.hs = None
+        
+    def forward(self, xs):
+        xs = self.embed.forward(xs)
+        hs = self.lstm.forward(xs)
+        self.hs = hs
+        return hs[:, -1, :]
+    
+    def backward(self, dh):
+        dhs = np.zeros_like(self.hs)
+        dhs[:, -1, :] = dh
+        
+        dout = self.lstm.backward(dhs)
+        dout = self.embed.backward(dout)
+        return dout
+    
+
+class Seq2seq(BaseModel):
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        self.encoder = Encoder(V, D, H)
+        self.decoder = Decoder(V, D, H)
+        self.softmax = TimeSoftmaxWithLoss()
+        
+        self.params = self.encoder.params + self.decoder.params
+        self.grads = self.encoder.grads + self.decoder.grads
+        
+    def forward(self, xs, ts):
+        decoder_xs, decoder_ts = ts[:, :-1], ts[:, 1:]
+        
+        h = self.encoder.forward(xs)
+        score = self.decoder.forward(decoder_xs, h)
+        loss = self.softmax.forward(score, decoder_ts)
+        return loss
+    
+    def backward(self, dout=1):
+        dout = self.softmax.backward(dout)
+        dh = self.decoder.backward(dout)
+        dout = self.encoder.backward(dh)
+        return dout
+    
+    def generate(self, xs, start_id, sample_size):
+        h = self.encoder.forward(xs)
+        sampled = self.decoder.generate(h, start_id, sample_size)
+        return sampled
